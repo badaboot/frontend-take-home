bee4671b6b984c9b09f5412f084e919a26f5b723
Anny He <annyannyhehe@gmail.com>
2024-08-21 10:10:00 -0700

    add CSS margin on mobile

--- src/App.js
+++ src/App.js
@@ -2,54 +2,57 @@ import "./styles.css";
 import { useState } from "react";
 
 async function getData(query) {
-  const url = `https://api.npms.io/v2/search/suggestions?q=${query}`;
+    const url = `https://api.npms.io/v2/search/suggestions?q=${query}`;
 
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error(error.message);
     }
-
-    return await response.json();
-  } catch (error) {
-    console.error(error.message);
-  }
 }
+
 export default function App() {
-  const [results, setResults] = useState([]);
-  const searchApi = async (e) => {
-    const query = e.target.value;
-    const data = await getData(query);
-    if (data !== undefined) {
-      setResults(data);
-    }
-  };
-  return (
-    <div className="App">
-      <input onBlur={searchApi} />
-      <div>
-        {results &&
-          results.map((o) => {
-            const { name, description, version, date } = o.package;
-            const { quality, popularity, maintenance } = o.score.detail;
-            return (
-              <div key={name} className="searchBox">
-                <p className="bold">{name}</p>
-                <div className="flex">
-                  <div>
-                    <p className="gray">{description}</p>
-                    <p>
-                      {o.package.publisher.username}{" "}
-                      <span className="gray">
-                        published {version} {date}
-                      </span>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-      </div>
-    </div>
-  );
+    const [loading, setLoading] = useState(false);
+    const [results, setResults] = useState([]);
+    const searchApi = async (e) => {
+        const query = e.target.value;
+        setLoading(true);
+        const data = await getData(query);
+        setLoading(false);
+        setResults(data);
+    };
+    return (
+        <div className="App">
+            <p>Search npm packages on blur</p>
+            <input onBlur={searchApi} />
+            <div>
+                {loading && <div>Loading</div>}
+                {!loading && results &&
+                    results.map((o) => {
+                        const { name, description, version, date } = o.package;
+                        return (
+                            <div key={name} className="searchBox">
+                                <p className="bold">{name}</p>
+                                <div className="flex">
+                                    <div>
+                                        <p className="gray">{description}</p>
+                                        <p>
+                                            {o.package.publisher.username}{" "}
+                                            <span className="gray">
+                                                published {version} {date}
+                                            </span>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+            </div>
+        </div>
+    );
 }
